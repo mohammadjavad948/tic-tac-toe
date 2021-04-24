819315ccf23282faf6bdfe38aa7bc2c81a1624ce
mohammadjavad <mjkmo123@gmail.com>
2021-04-24 09:47:41 +0430

    import socket io

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+const socketIO = require('socket.io');
 
 const app = express();
 
 const server = app.listen(process.env || 3000, () => {
     console.log('server is running');
 });
+
+const io = socketIO(server);
