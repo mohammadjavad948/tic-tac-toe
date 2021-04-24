a427136e00a560396b9a995b966d2c3a4f93bf5b
mohammadjavad <mjkmo123@gmail.com>
2021-04-24 16:10:01 +0430

    add join room event listener

--- server/src/roomManager.ts
+++ server/src/roomManager.ts
@@ -5,12 +5,14 @@ import {generateID} from "./generateId";
 
 export async function registerRoomManager(io, socket: Socket, rooms: Map<string, RoomInterface>){
 
+    // get all rooms
     socket.on('rooms:all', (callback: any) => {
         callback({
             rooms: rooms.values()
         });
     });
 
+    // create new room
     socket.on('room:create', (name: string, callback: any) => {
         // check if room with that name exists return error
         if (rooms.has(name)) {
@@ -48,6 +50,11 @@ export async function registerRoomManager(io, socket: Socket, rooms: Map<string,
             ok: true,
             room: newRoom
         })
+    });
+
+    // join new room
+    socket.on('room:join', (name: string, callback: any) => {
+
     })
 
 }
