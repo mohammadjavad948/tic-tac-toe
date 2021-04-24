dae69acb9f02eca9334de390b6f8bc7de357289b
mohammadjavad <mjkmo123@gmail.com>
2021-04-24 10:21:16 +0430

    create some functions

--- server/src/connectionManager.ts
+++ server/src/connectionManager.ts
@@ -3,4 +3,11 @@ import {RoomInterface} from "./roomInterface";
 
 export async function registerConnectionManager(io, socket: Socket, room: Map<string, RoomInterface>){
 
+    socket.on('disconnect', () => {
+
+    });
+
+    socket.on('register:name', (name: string, callback: any) => {
+
+    });
 }
