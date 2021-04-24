3dc9edaf22537891a6564127f5307611b70d4200
mohammadjavad <mjkmo123@gmail.com>
2021-04-24 10:17:43 +0430

    add register function

---
+++ server/src/connectionManager.ts
@@ -0,0 +1,6 @@
+import {Socket} from "socket.io";
+import {RoomInterface} from "./roomInterface";
+
+export async function registerConnectionManager(io, socket: Socket, room: Map<string, RoomInterface>){
+
+}
