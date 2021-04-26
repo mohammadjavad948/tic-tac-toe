24bcd965df8792ba9e787d2610169ade1fcc8c82
mohammadjavad <mjkmo123@gmail.com>
2021-04-26 15:00:18 +0430

    add simple router

--- web/src/Container.tsx
+++ web/src/Container.tsx
@@ -1,11 +1,20 @@
 import style from './container.module.css';
 import AskName from "./AskName";
+import {useNameStore} from "./nameStore";
 
 export default function Container(){
 
+    const {name} = useNameStore()
+
+    function showComponent(){
+        if (name === ''){
+            return <AskName />
+        }
+    }
+
     return (
         <div className={style.container}>
-            <AskName />
+            {showComponent()}
         </div>
     )
 }
