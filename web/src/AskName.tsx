f75d1c20a6623e4d6740b707cc4a803c03f0a294
mohammadjavad <mjkmo123@gmail.com>
2021-04-26 14:57:10 +0430

    add setName

--- web/src/AskName.tsx
+++ web/src/AskName.tsx
@@ -1,8 +1,14 @@
 import style from './askName.module.css';
 import {Button, TextField, Typography} from "@material-ui/core";
 import {useSpring, a} from 'react-spring';
+import {useNameStore} from "./nameStore";
+import {useState} from "react";
 
 export default function AskName(){
+
+    const { setName: setNewName } = useNameStore()
+
+    const [name, setName] = useState("");
 
     const animation = useSpring({
         from: {
@@ -13,12 +19,20 @@ export default function AskName(){
         }
     })
 
+    function change(e: any){
+        setName(e.target.value);
+    }
+
+    function save(){
+        setNewName(name);
+    }
+
     return (
         <a.div className={style.container} style={animation}>
             <Typography variant={"h6"}>Hi. What’s your name?</Typography>
             <div className={style.form}>
-                <TextField label={"name"} variant={"outlined"}/>
-                <Button variant={"contained"} color={"primary"}>Go</Button>
+                <TextField label={"name"} variant={"outlined"} onChange={change}/>
+                <Button variant={"contained"} onClick={save} color={"primary"}>Go</Button>
             </div>
         </a.div>
     )
