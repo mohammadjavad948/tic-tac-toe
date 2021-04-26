fa2864ec564aa876583df3fea665032754913514
mohammadjavad <mjkmo123@gmail.com>
2021-04-26 14:38:32 +0430

    add typography

--- web/src/AskName.tsx
+++ web/src/AskName.tsx
@@ -1,10 +1,11 @@
 import style from './askName.module.css';
+import {Typography} from "@material-ui/core";
 
 export default function AskName(){
 
     return (
         <div className={style.container}>
-
+            <Typography variant={"h6"}>Hi. What’s your name?</Typography>
         </div>
     )
 }
