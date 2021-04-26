d9f8f52738c2b9cb65d3aa0368d7ed82f49d0091
mohammadjavad <mjkmo123@gmail.com>
2021-04-26 14:48:47 +0430

    animations goes brrrr

--- web/src/AskName.tsx
+++ web/src/AskName.tsx
@@ -1,15 +1,25 @@
 import style from './askName.module.css';
 import {Button, TextField, Typography} from "@material-ui/core";
+import {useSpring, a} from 'react-spring';
 
 export default function AskName(){
 
+    const animation = useSpring({
+        from: {
+            opacity: 0
+        },
+        to: {
+            opacity: 1
+        }
+    })
+
     return (
-        <div className={style.container}>
+        <a.div className={style.container} style={animation}>
             <Typography variant={"h6"}>Hi. What’s your name?</Typography>
             <div className={style.form}>
                 <TextField label={"name"} variant={"outlined"}/>
                 <Button variant={"contained"} color={"primary"}>Go</Button>
             </div>
-        </div>
+        </a.div>
     )
 }
