0513a4f156dfa658fe3b1bac07441618ffbc5a0d
mohammadjavad <mjkmo123@gmail.com>
2021-04-26 14:34:58 +0430

    add askname

--- web/src/Container.tsx
+++ web/src/Container.tsx
@@ -1,10 +1,11 @@
 import style from './container.module.css';
+import AskName from "./AskName";
 
 export default function Container(){
 
     return (
         <div className={style.container}>
-
+            <AskName />
         </div>
     )
 }
