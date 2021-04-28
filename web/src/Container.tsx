764835840a30168ac9e4237c9356b4ff883e6f69
mohammadjavad <mjkmo123@gmail.com>
2021-04-28 14:47:26 +0430

    simple connection

--- web/src/Container.tsx
+++ web/src/Container.tsx
@@ -2,10 +2,23 @@ import style from './container.module.css';
 import AskName from "./AskName";
 import {useNameStore} from "./nameStore";
 import RoomSelection from "./RoomSelection";
+import {useEffect} from "react";
+import {io} from "socket.io-client";
+
+const socket = io('https://secret-fortress-87428.herokuapp.com/');
 
 export default function Container(){
 
     const {name} = useNameStore()
+
+    useEffect(() => {
+
+        socket.on('connect', () => {
+            console.log(socket.id)
+        })
+
+    }, [])
+
 
     function showComponent(){
         if (name === ''){
