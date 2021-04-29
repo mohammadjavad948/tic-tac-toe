d4271162b13f1f66596d63c747acab64a6f73a3b
mohammadjavad <mjkmo123@gmail.com>
2021-04-29 13:31:44 +0430

    create Game Component

---
+++ web/src/Game.tsx
@@ -0,0 +1,15 @@
+import {Socket} from "socket.io-client";
+import {FC} from "react";
+
+interface Prop{
+    socket: Socket
+}
+
+export const Game: FC<Prop> = (prop) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
