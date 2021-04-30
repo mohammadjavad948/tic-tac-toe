b6f79e860c4b5ee8096c39cf226e5879c5af780d
mohammadjavad <mjkmo123@gmail.com>
2021-04-30 14:03:49 +0430

    more method

--- web/src/GameStore.ts
+++ web/src/GameStore.ts
@@ -21,10 +21,26 @@ interface PlayerStoreI extends State{
         id: string
         role: 'X' | 'O' | 'observer'
     }[],
+    setPlayers: (player: any) => void,
+    addPlayer: (player: any) => void,
+    removePlayer: (id: string) => void
 }
 
 export const usePlayerStore = create<PlayerStoreI>(set => {
     return {
-        players: []
+        players: [],
+        setPlayers: (player) => {
+            set({players: player})
+        },
+        addPlayer: (player) => {
+            set(state => {
+                return {players: state.players.concat(player)}
+            })
+        },
+        removePlayer: (id) => {
+            set(state => {
+                return {players: state.players.filter(e => e.id !== id)}
+            })
+        }
     }
 })
