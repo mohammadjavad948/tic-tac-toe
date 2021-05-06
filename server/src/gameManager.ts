2345d10d28d91c9bff1d4ea8f465b958f314b444
mohammadjavad <mjkmo123@gmail.com>
2021-05-06 09:27:25 +0430

    register move event

--- server/src/gameManager.ts
+++ server/src/gameManager.ts
@@ -3,5 +3,12 @@ import {RoomInterface} from "./roomInterface";
 
 
 export async function registerGameManager(io, socket: Socket, rooms: Map<string, RoomInterface>){
+    // register events
+    socket.on('game:move', move);
+    // socket.on('game:reaction');
 
+    // event functions
+    function move(block: number){
+
+    }
 }
