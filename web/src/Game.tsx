708d11ab55e3ea57d967e784d1e14a1ee7a88033
mohammadjavad <mjkmo123@gmail.com>
2021-05-07 08:42:45 +0430

    move player

--- web/src/Game.tsx
+++ web/src/Game.tsx
@@ -1,9 +1,8 @@
 import {Socket} from "socket.io-client";
 import {FC, useEffect, useRef, useState} from "react";
 import styles from './game.module.css';
-import {Icon, Typography} from "@material-ui/core";
+import {Typography} from "@material-ui/core";
 import {usePlayerStore} from "./GameStore";
-import {useTransition, animated} from "react-spring";
 
 interface Prop{
     socket: Socket
@@ -30,7 +29,6 @@ export const Game: FC<Prop> = (prop) => {
         <div className={styles.container}>
             <Title />
             <Board />
-            <Players />
         </div>
     )
 }
@@ -58,59 +56,6 @@ function Board(){
             <div className={styles.cell}> </div>
             <div className={styles.cell}> </div>
         </div>
-    )
-}
-
-function Players(){
-
-    const {players} = usePlayerStore();
-
-    const transition = useTransition(
-        players,
-        {
-            from: (item, index) => {
-                return { y: index * 60, opacity: 0 }
-            },
-            leave: (item, index) => {
-                return { y: index * 60, opacity: 0 }
-            },
-            enter: (item, index) => {
-                return { y: index * 70, opacity: 1 }
-            },
-            update: (item, index) => {
-                return { y: index * 70 }
-            },
-        }
-    );
-
-    return (
-        <div style={{width: '100%', position: 'relative'}}>
-            <Typography variant={"h6"}>
-                Players
-            </Typography>
-            {transition((style, item) => (
-                <PlayerCard player={item} style={style}/>
-            ))}
-        </div>
-    )
-}
-
-// @ts-ignore
-function PlayerCard({player, style}){
-
-    return (
-        <animated.div className={styles.playerCard} style={{top: style.y.to((x: any) => (x + 28) + 'px'), opacity: style.opacity}}>
-            <span className={styles.playerLogo}>
-                {
-                    player.role !== 'observer' ?
-                        player.role :
-                        <Icon>
-                            visibility
-                        </Icon>
-                }
-            </span>
-            <span>{player.name}</span>
-        </animated.div>
     )
 }
 
