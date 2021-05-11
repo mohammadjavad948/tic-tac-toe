5748cd342fa124893911411387356d6c771f788e
mohammadjavad <mjkmo123@gmail.com>
2021-05-11 18:58:12 +0430

    title animation done

--- web/src/TitleAnimation.tsx
+++ web/src/TitleAnimation.tsx
@@ -1,11 +1,31 @@
 import {FC} from "react";
 import style from './titleAnimation.module.css';
+import {useTransition, a} from "react-spring";
 
 export const TitleAnimation: FC = (props) => {
 
+    const animation = useTransition([props.children], {
+       from: {
+           y: -20,
+           opacity: 0
+       },
+       enter: {
+           y: 0,
+           opacity: 1
+       },
+       leave: {
+           y: 20,
+           opacity: 0
+       }
+    });
+
     return (
         <div className={style.container}>
-
+            {animation((springStyle,item) => {
+                return <a.div style={springStyle} className={style.item}>
+                        {item}
+                </a.div>;
+            })}
         </div>
     )
 }
