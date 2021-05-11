806619642b779dca69c11786ca132bb638be1a2f
mohammadjavad <mjkmo123@gmail.com>
2021-05-11 18:52:09 +0430

    title animation style

--- web/src/TitleAnimation.tsx
+++ web/src/TitleAnimation.tsx
@@ -1,10 +1,10 @@
 import {FC} from "react";
-
+import style from './titleAnimation.module.css';
 
 export const TitleAnimation: FC = (props) => {
 
     return (
-        <div>
+        <div className={style.container}>
 
         </div>
     )
