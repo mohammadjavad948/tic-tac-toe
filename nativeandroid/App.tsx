f57a9dec3440eddae9680af5c3cba2e022645cd6
mohammadjavad <mjkmo123@gmail.com>
2021-05-15 16:41:00 +0430

    hide splash screen

--- nativeandroid/App.tsx
+++ nativeandroid/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {Provider as PaperProvider } from "react-native-paper";
 import { AskName } from "./AskName";
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <PaperProvider>
