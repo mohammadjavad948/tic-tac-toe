724891de969ceba348a97ae043e70bdbdd6a08f1
mohammadjavad <mjkmo123@gmail.com>
2021-05-15 22:44:57 +0430

    Revert "add routing"
    This reverts commit 5add17c9

--- nativeandroid/App.tsx
+++ nativeandroid/App.tsx
@@ -2,44 +2,17 @@ import React, { useEffect } from "react";
 import {Provider as PaperProvider } from "react-native-paper";
 import { AskName } from "./AskName";
 import SplashScreen from 'react-native-splash-screen';
-import { NativeRouter, useLocation, Switch, Route } from "react-router-native";
-import { useTransition, animated }from 'react-spring/native';
-import { View } from "react-native";
+import { NativeRouter } from "react-router-native";
 
 export default function App() {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
-  const location = useLocation();
-  const transitions = useTransition(location, {
-    from: {
-      right: -100,
-      opacity: 0,
-    },
-    enter: {
-      right: 0,
-      opacity: 1,
-    },
-    leave: {
-      right: 100,
-      opacity: 0,
-    },
-  });
-
   return (
     <NativeRouter>
       <PaperProvider>
-        <View style={{position: 'relative', flex: 1}}>
-          {transitions((props, item) => (
-            <animated.View style={[{position: 'absolute'}, props]}>
-              <Switch location={item}>
-                <Route path="/" exact component={AskName} />
-              </Switch>
-            </animated.View>
-          ))}
-        </View>
-
+        <AskName />
       </PaperProvider>
     </NativeRouter>
   );
