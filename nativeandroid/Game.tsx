ecbcd017fc5f60f3c69b7b526501ffb14c7b0879
mohammadjavad <mjkmo123@gmail.com>
2021-05-21 19:39:46 +0430

    style

--- nativeandroid/Game.tsx
+++ nativeandroid/Game.tsx
@@ -1,10 +1,11 @@
 import {View} from 'react-native';
 import {Text} from 'react-native-paper';
 import React from 'react';
+import {styles} from './gameStyle';
 
 export default function Game() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>game</Text>
     </View>
   );
