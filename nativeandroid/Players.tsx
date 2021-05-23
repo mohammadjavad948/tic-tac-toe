aaa49f6ad156b1271a738c72a0d5758ea7a46999
mohammadjavad <mjkmo123@gmail.com>
2021-05-23 20:50:51 +0430

    add Player component

---
+++ nativeandroid/Players.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from "react-native";
+import { playerStyle } from "./playersStyle";
+
+
+export function PlayerContainer(){
+
+
+  return (
+    <View style={playerStyle.container}>
+
+    </View>
+  );
+}
---
+++ nativeandroid/playersStyle.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const playerStyle = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+  }
+})
