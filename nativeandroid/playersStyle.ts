cb7c846d08c4a11cd42a7fce548bbc7a5db505f9
mohammadjavad <mjkmo123@gmail.com>
2021-05-23 21:03:03 +0430

    bug fix

--- nativeandroid/playersStyle.ts
+++ nativeandroid/playersStyle.ts
@@ -1,13 +1,22 @@
-import {StyleSheet} from 'react-native';
+import { Appearance, StyleSheet } from "react-native";
+
+const theme = Appearance.getColorScheme();
 
 export const playerStyle = StyleSheet.create({
   container: {
     position: 'absolute',
     bottom: 0,
     right: 0,
+    width: '100%',
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center"
   },
   bilbilakContainer: {
     width: '100%',
-    height: 20,
+    height: 25,
+    backgroundColor: theme === 'light' ? '#ececec' : '#5a5a5a',
+    borderTopRightRadius: 8,
+    borderTopLeftRadius: 8,
   },
 });
