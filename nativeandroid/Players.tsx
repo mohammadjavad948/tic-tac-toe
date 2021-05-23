e115656c9134b21e17df5656c96de00a7a5f904c
mohammadjavad <mjkmo123@gmail.com>
2021-05-23 20:52:59 +0430

    bilbilak container

--- nativeandroid/Players.tsx
+++ nativeandroid/Players.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { View } from "react-native";
-import { playerStyle } from "./playersStyle";
+import {View} from 'react-native';
+import {playerStyle} from './playersStyle';
 
-
-export function PlayerContainer(){
-
-
+export function PlayerContainer() {
   return (
     <View style={playerStyle.container}>
+      <BilBilak />
+    </View>
+  );
+}
 
+function BilBilak() {
+  return (
+    <View style={playerStyle.bilbilakContainer}>
+      <View />
     </View>
   );
 }
--- nativeandroid/playersStyle.ts
+++ nativeandroid/playersStyle.ts
@@ -1,9 +1,13 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
 
 export const playerStyle = StyleSheet.create({
   container: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 0,
     right: 0,
-  }
-})
+  },
+  bilbilakContainer: {
+    width: '100%',
+    height: 20,
+  },
+});
