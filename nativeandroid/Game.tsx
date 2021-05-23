bf9428a8de1304bc47fb93810735f7ca5e2ff490
mohammadjavad <mjkmo123@gmail.com>
2021-05-23 11:57:27 +0430

    add tiles

--- nativeandroid/Game.tsx
+++ nativeandroid/Game.tsx
@@ -1,12 +1,40 @@
-import {View} from 'react-native';
+import {Dimensions, View} from 'react-native';
 import {Text} from 'react-native-paper';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {styles} from './gameStyle';
+import {useBoardStore} from './GameStore';
 
 export default function Game() {
   return (
     <View style={styles.container}>
-      <Text>game</Text>
+      <Board />
+    </View>
+  );
+}
+
+function Board() {
+  const [width, setWidth] = useState(0);
+  const {board} = useBoardStore();
+
+  useEffect(() => {
+    const w = Dimensions.get('window').width;
+
+    if (w > 600) {
+      setWidth((600 - 20) / 3);
+    } else {
+      setWidth((w - 20) / 3);
+    }
+  }, []);
+
+  return (
+    <View style={styles.board}>
+      {board.map((el, index) => {
+        return (
+          <View key={index} style={[{width, height: width}, styles.tile]}>
+            {el}
+          </View>
+        );
+      })}
     </View>
   );
 }
--- nativeandroid/gameStyle.ts
+++ nativeandroid/gameStyle.ts
@@ -5,4 +5,21 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+  board: {
+    maxWidth: 600,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+  },
+  tile: {
+    borderColor: 'white',
+    borderRadius: 5,
+    marginLeft: 2.5,
+    marginRight: 2.5,
+    marginTop: 5,
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
 });
